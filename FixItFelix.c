f9b841d2fcb4f5d24d93dd84f9464b10c0addfc3
Caleb Dehaan <caleb.dehaan@outlook.com>
2024-07-08 15:43:39 -0400

    07/08 v2

--- FixItFelix.c
+++ FixItFelix.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
-// Function to search for a value in an array and perform an action if found
-void searchAndPerformAction(int arr[], int size, int target) {
+
+// Function to search for multiple values in an array and perform actions if found
+void searchAndPerformActions(int arr[], int size, int targets[], int targetSize) {
     int i;
-    int found = 0; // Flag to indicate if the value is found
+    int o1 = 1;
+    int o2 = 2;
+    int o3 = 3;
+    int o4 = 4;
+    int o5 = 5;
+    int o6 = 6;
+    int o7 = 7;
+    int o8 = 8;
+    int o9 = 9;
+    int o10 = 10;
+    int o11 = 11;
+    int o12 = 12;
+    int o13 = 13;
+    int o14 = 14;
 
-    for (i = 0; i < size; i++) { //Searches array and checks if current element is equal to target value
-        if (arr[i] == target) {
-            found = 1;
-            // Perform the desired action
-            printf("Value %d found at index %d. Performing action...\n", target, i);
-            // Add any other actions you want to perform here
+    for (i = 0; i < targetSize; i++) {
+        int target = targets[i];
+        int found = 0; // Flag to indicate if the value is found
+        
+        for (j = 0; j < size; j++) {
+            if (arr[j] == target) {
+                found = 1;
+                // Perform the desired action
+                printf("Value %d found at index %d. Performing action...\n", target, j);
+                // Add any other actions you want to perform here
+            }
         }
-    }
-
-    if (!found) {
-        printf("Value %d not found in the array.\n", target);
+        
+        if (!found) {
+            printf("Value %d not found in the array.\n", target);
+        }
     }
 }
 
@@ -22,10 +41,13 @@ void searchAndPerformAction(int arr[], int size, int target) {
 int main(){ 
 
     // Var for numbers of tools to be run
-    int numtools, i;
+    int n_tools, n_options, n_selected, i;
 
-    // Declare an array to store the numbers
-    int arr[n];
+    // Declare an array to store the numbers of the menu options
+    int arr[n_options] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    // Declare an array to store the target values
+    int targets[n_selected];
 
     printf("Welcome to FixItFelix");    
     printf("What do we want to fix today?:\n");
@@ -45,21 +67,14 @@ int main(){
     printf("14. Reboot PC\n");
 
     // Prompt the user to enter each number for desired tools to be run
-    printf("Enter %d numbers:\n", numtools);
-    for (i = 0; i < n; i++) {
-        printf("Enter number for tool you would like to run: %d: ", i + 1);
-        scanf("%d", &arr[i]);
+    printf("Enter %d numbers:\n", n_selected);
+    for(i = 0; i < m; i++) {
+        printf("Enter number for tools you would like to run %d: ", i + 1);
+        scanf("%d", &targets[i]);
     }
 
-    // Optionally, print to verify the input
-    printf("You entered the following numbers:\n");
-    for (i = 0; i < numtools; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    // Call the function to search for the value and perform an action if found
-    searchAndPerformAction(arr, n, target);
+    // Call the function to search for the target values and perform actions if found
+    searchAndPerformActions(arr, n, targets, m);
 
  return 0; 
-}  
+}   
